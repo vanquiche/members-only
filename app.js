36ad66fbc02c59e991add69078d4f6fe2b2c5841
vanquiche <stevevang2010@hotmail.com>
2021-07-31 20:09:09 -0700

    add authentication and pw hashing

--- app.js
+++ app.js
@@ -7,26 +7,24 @@ var logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+const flash = require('connect-flash');
+const bcrypt = require('bcryptjs');
+
+const User = require('./models/userModel');
 
 // IMPORT ROUTE FILES
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
+var userRouter = require('./routes/userRoute');
 var messagesRouter = require('./routes/messages');
 var profileRouter = require('./routes/profile');
 
 var app = express();
 
-//Import the mongoose module
-var mongoose = require('mongoose');
-
-//Set up default mongoose connection
+// MONGO DB CONNECTION
 var mongoDB = process.env.DB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//Get the default connection
 var db = mongoose.connection;
-
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -34,15 +32,57 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// passport auth strategy
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
+    });
+  })
+);
+// passport session auth
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// express config
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
+
+// routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
+app.use('/user', userRouter);
 app.use('/messages', messagesRouter);
 app.use('/profile', profileRouter);
 
