889a5432ca4381b25c1436094d9ffe661d22216b
vanquiche <stevevang2010@hotmail.com>
2021-08-02 23:41:40 -0700

    update schema

--- models/messageModel.js
+++ models/messageModel.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
   message: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, required: true, ref: User },
-  date: { type: Date, default: Date.now, required: true },
+  author: { type: Schema.Types.ObjectId, required: true},
+  createdAt: { type: Date, default: Date.now, required: true },
 });
 
 module.exports = mongoose.model('Message', messageSchema);
