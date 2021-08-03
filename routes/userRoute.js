abf9a32a3faf1eeda88b593c2c7b315405c36e2a
vanquiche <stevevang2010@hotmail.com>
2021-08-02 23:43:21 -0700

    adjusted views

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,8 +11,7 @@ router.post('/sign-up', user_controller.user_signUp_post);
 router.get('/login', user_controller.user_login_get);
 router.post('/login', user_controller.user_login_post);
 
-router.get('/logout', user_controller.user_logout_post);
-
 router.get('/profile', user_controller.user_profile_get);
+router.post('/profile', user_controller.user_logout_post);
 
 module.exports = router;
